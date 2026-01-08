8b5475e3d7f63e30e0434213ff86173b486f0a19
Heker22 <146105661+Heker22@users.noreply.github.com>
2026-01-08 15:39:29 +0100

    s

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 
-import { customAlphabet } from "nanoid";
+/*import { customAlphabet } from "nanoid";
 const id = customAlphabet('abcdefghilm', 4);
 
-console.log(id());
+console.log(id());*/
+
+/*line graphic*/
+
+import {Chart} from 'chart.js'
+
+const lineData = {
+    labels:['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datsets: [{
+        label: 'Temperature',
+        data: [3, 4, 9, 16, 20, 25, 28],
+        fill: false,
+        borderColor: 'rgb(75.192.192)',
+        tension:0.1
+    }],
+    
+}
+
+const lineConfig = {
+    type: 'line',
+    data: lineData,
+    options: {
+        responsive: true,
+    }
+
+}
+
+const ctx = document.getElementById('lineChart')
+
+
+new Chart(
+    ctx,
+    lineConfig
+)
