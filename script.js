4f71d451ef10389277589f5a14f8f53fecde130e
Heker22 <146105661+Heker22@users.noreply.github.com>
2026-01-14 15:31:53 +0100

    ew

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log(id());*/
 
 /*line graphic*/
 
-/*import Chart from 'chart.js/auto'*/
+/*import Chart from 'chart.js/auto'
 
 const lineData = {
     labels:['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -35,4 +35,67 @@ const ctx = document.getElementById('lineChart')
 new Chart(
     ctx,
     lineConfig
+)*/
+
+
+/*cirlce diagram
+
+const data = {
+    labels: ['Оренда', 'Їжа', 'Транспорт', 'Розваги'],
+    datasets: [{
+        label: 'Витрати',
+        data: [800, 500, 150, 300],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }
+}
+
+const ctx = document.getElementById('doughnutChart')
+
+
+new Chart(
+    ctx,
+    config
 )
+
+*/
+
+/*basiclightbox*/
+
+/* ex 1
+const img = `<img src="https://images.pexels.com/photos/16330147/pexels-photo-16330147.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="book">`
+
+const image = basicLightbox.create(img);
+
+image.show();
+*/
+
+
+const html = '<h1>LETSGOSKI</h1>';
+
+const text = basicLightbox.create(html, {
+    width: '500px',
+    height: '300px',
+    backgroundColor: 'red',
+    
+})
+
+text.show();
